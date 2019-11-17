62e2f9d11ed99c8bc78acfd288a084d0cb3dfd6a
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-17 19:54:36 +0100

    moved AntiSpin class in .h

--- main/main_antispin.cpp
+++ main/main_antispin.cpp
@@ -12,36 +12,6 @@
 #include <list>
 #include <set>
 
-class AntiSpin {
-public:
-    AntiSpin(int window_ms) {
-        this->window_ms = window_ms;
-        last_execute_millis = 0;
-    }
-
-    AntiSpin &setOnExecuteLambda(std::function<void()> l) {
-        this->onExecuteLambda = l;
-        return *this;
-    }
-
-    bool spin() {
-        auto ms = millis();
-        auto delta = ms - last_execute_millis;
-        if (counter == 0 || delta > window_ms) {
-            counter++;
-            last_execute_millis = ms;
-            onExecuteLambda();
-        }
-
-    }
-
-    unsigned long counter = 0;
-protected:
-    std::function<void()> onExecuteLambda;
-    unsigned long last_execute_millis;
-
-    int window_ms;
-};
 
 int main() {
     AntiSpin x(1000);
--- ospf_mini_lib.h
+++ ospf_mini_lib.h
@@ -131,5 +131,36 @@ public:
 
 };
 
+class AntiSpin {
+public:
+    AntiSpin(int window_ms) {
+        this->window_ms = window_ms;
+        last_execute_millis = 0;
+    }
+
+    AntiSpin &setOnExecuteLambda(std::function<void()> l) {
+        this->onExecuteLambda = l;
+        return *this;
+    }
+
+    bool spin() {
+        auto ms = millis();
+        auto delta = ms - last_execute_millis;
+        if (counter == 0 || delta > window_ms) {
+            counter++;
+            last_execute_millis = ms;
+            onExecuteLambda();
+        }
+
+    }
+
+    unsigned long counter = 0;
+protected:
+    std::function<void()> onExecuteLambda;
+    unsigned long last_execute_millis;
+
+    int window_ms;
+};
+
 
 #endif //_OSPF_MINI_LIB_H_
