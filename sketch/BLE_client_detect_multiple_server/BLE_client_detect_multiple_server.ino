45913114dfe8c7ed34ccef52f00fd1fb23b0e74a
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-17 19:35:10 +0100

    better client

--- sketch/BLE_client_detect_multiple_server/BLE_client_detect_multiple_server.ino
+++ sketch/BLE_client_detect_multiple_server/BLE_client_detect_multiple_server.ino
@@ -2,7 +2,6 @@
 #include "mbed.h"
 #include <ArduinoBLE.h>
 #include "ospf_mini_lib.h"
-
 #else
 //for CLion compilation
 #include "../../ospf_mini_lib.h"
@@ -10,121 +9,119 @@
 #include "../../ArduinoBLE/src/ArduinoBLE.h"
 #endif
 
-#include <list>
 void setup() {
-  Serial.begin(9600);
-  //while (!Serial);
+    Serial.begin(9600);
+    while (!Serial);
 
-  // begin initialization
-  if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
+    // begin initialization
+    if (!BLE.begin()) {
+        Serial.println("starting BLE failed!");
 
-    while (1);
-  }
-
-  Serial.println("BLE Central - SensorTag button");
-  Serial.println("Make sure to turn on the device.");
-
-  // start scanning for peripheral
-  BLE.scan();
-}
-std::list<BLEDevice> servers;
-void loop() {
-
-  BLE.poll();
-  
-  // check if a peripheral has been discovered
-  BLEDevice peripheral = BLE.available();
-  if (peripheral) {
-    // discovered a peripheral, print out address, local name, and advertised service
-    Serial.print("Found ");
-    Serial.print(peripheral.address());
-    Serial.print(" '");
-    Serial.print(peripheral.localName());
-    Serial.print("' ");
-    Serial.print(peripheral.advertisedServiceUuid());
-    Serial.println();
-
-    // Check if the peripheral is a SensorTag, the local name will be:
-    // "CC2650 SensorTag"
-    if (peripheral.advertisedServiceUuid().equalsIgnoreCase("1234")) {
-      // stop scanning
-//      BLE.stopScan();
-
-      monitorSensorTagButtons(peripheral);
-
-      // peripheral disconnected, start scanning again
-      Serial.println("Starting scan again");
-      adv = true;
-      peripheral.disconnect();
-      //BLE.scan();
+        while (1);
     }
-  }
+
+    Serial.println("BLE Central - SensorTag button");
+    Serial.println("Make sure to turn on the device.");
+
+    // start scanning for peripheral
+    BLE.scan();
+}
+
+void loop() {
+    // check if a peripheral has been discovered
+    BLEDevice peripheral = BLE.available();
+
+    if (peripheral) {
+        // discovered a peripheral, print out address, local name, and advertised service
+        Serial.print("Found ");
+        Serial.print(peripheral.address());
+        Serial.print(" '");
+        Serial.print(peripheral.localName());
+        Serial.print("' ");
+        Serial.print(peripheral.advertisedServiceUuid());
+        Serial.println();
+
+        // Check if the peripheral is a SensorTag, the local name will be:
+        // "CC2650 SensorTag"
+        if (peripheral.advertisedServiceUuid().startsWith("1234")) {
+            // stop scanning
+            BLE.stopScan();
+
+            monitorSensorTagButtons(peripheral);
+
+            // peripheral disconnected, start scanning again
+            BLE.scan();
+        }
+    }
 }
 
 void monitorSensorTagButtons(BLEDevice peripheral) {
-  // connect to the peripheral
-  Serial.println("Connecting ...");
-  if (peripheral.connect()) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Failed to connect!");
-    return;
-  }
-
-  // discover peripheral attributes
-  Serial.println("Discovering service 1234 ...");
-  if (peripheral.discoverService("1234")) {
-    Serial.println("Service discovered");
-  } else {
-    Serial.println("Attribute discovery failed.");
-    peripheral.disconnect();
-    //    while (1);
-    return;
-  }
-
-  // retrieve the simple key characteristic
-  BLECharacteristic simpleKeyCharacteristic = peripheral.characteristic("2A19");
-
-  // subscribe to the simple key characteristic
-  Serial.println("Subscribing to simple key characteristic ...");
-  if (!simpleKeyCharacteristic) {
-    Serial.println("no simple key characteristic found!");
-    peripheral.disconnect();
-    return;
-  } else if (!simpleKeyCharacteristic.canSubscribe()) {
-    Serial.println("simple key characteristic is not subscribable!");
-    peripheral.disconnect();
-    return;
-  } else if (!simpleKeyCharacteristic.subscribe()) {
-    Serial.println("subscription failed!");
-    peripheral.disconnect();
-    return;
-  } else {
-    Serial.println("Subscribed");
-    Serial.println("Press the right and left buttons on your SensorTag.");
-  }
-
-  //while (peripheral.connected()) {
-  if (peripheral.connected()) {
-    // while the peripheral is connected
-
-    // check if the value of the simple key characteristic has been updated
-    if (simpleKeyCharacteristic.valueUpdated()) {
-      // yes, get the value, characteristic is 1 byte so use byte value
-      byte value = 0;
-
-      simpleKeyCharacteristic.readValue(value);
-
-      Serial.print(peripheral.localName());
-      Serial.print(",");
-      Serial.print(value);
-      Serial.print(",");
-      Serial.print(peripheral.rssi());
-      Serial.println("");
-
+    // connect to the peripheral
+    Serial.println("Connecting ...");
+    if (peripheral.connect()) {
+        Serial.println("Connected");
+    } else {
+        Serial.println("Failed to connect!");
+        return;
     }
-  }
 
-  Serial.println("SensorTag disconnected!");
+    // discover peripheral attributes
+    Serial.println("Discovering service 1234 ...");
+    if (peripheral.discoverService("1234")) {
+        Serial.println("Service discovered");
+    } else {
+        Serial.println("Attribute discovery failed.");
+        peripheral.disconnect();
+        return;
+    }
+
+    // retrieve the simple key characteristic
+    BLECharacteristic simpleKeyCharacteristic = peripheral.characteristic("2A19");
+
+    // subscribe to the simple key characteristic
+    Serial.println("Subscribing to simple key characteristic ...");
+    if (!simpleKeyCharacteristic) {
+        Serial.println("no simple key characteristic found!");
+        peripheral.disconnect();
+        return;
+    } else if (!simpleKeyCharacteristic.canSubscribe()) {
+        Serial.println("simple key characteristic is not subscribable!");
+        peripheral.disconnect();
+        return;
+    } else if (!simpleKeyCharacteristic.subscribe()) {
+        Serial.println("subscription failed!");
+        peripheral.disconnect();
+        return;
+    } else {
+        Serial.println("Subscribed");
+        Serial.println("Press the right and left buttons on your SensorTag.");
+    }
+    int count = 0;
+    auto enter_millis = millis();
+    // while the peripheral is connected
+    while (peripheral.connected() && count < 15) {
+        auto delta = (millis() - enter_millis) / 1000;
+        if (delta > 10) {
+            printf("Timeout looping device. exiting loop\n");
+            break;
+        }
+
+        // check if the value of the simple key characteristic has been updated
+        if (simpleKeyCharacteristic.valueUpdated()) {
+            // yes, get the value, characteristic is 1 byte so use byte value
+            byte value = 0;
+            count++;
+            simpleKeyCharacteristic.readValue(value);
+
+            Serial.print(peripheral.localName());
+            Serial.print(",");
+            Serial.print(value);
+            Serial.print(",");
+            Serial.print(peripheral.rssi());
+            Serial.println("");
+        }
+    }
+    peripheral.disconnect();
+
+    Serial.println("SensorTag disconnected!");
 }
