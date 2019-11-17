0b3eb86f2e5000259c3cfc9f041e1b042ea736be
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-17 20:16:07 +0100

    printf redirection to serial

--- ospf_mini_dep.cpp
+++ ospf_mini_dep.cpp
@@ -5,6 +5,35 @@
 
 #ifdef ARDUINO_ARCH_MBED
 #include <Arduino.h>
+
+#include "mbed.h"
+
+//////////////////////////  printf redirection to serial START //////////////////////////
+namespace mbed
+{
+
+class RedirSerial : public FileHandle {
+  public:
+    RedirSerial() {}
+    ssize_t read(void *buffer, size_t size) override {
+      return 0;
+    }
+    ssize_t write(const void *buffer, size_t size)  override {
+      Serial.write((const uint8_t*)buffer, size);
+      return size;
+    }
+    off_t seek(off_t offset, int whence = SEEK_SET) override {}
+    int close() override {}
+};
+
+FileHandle *mbed_target_override_console(int)
+{
+  static RedirSerial res;
+  return &res;
+}
+
+} //namespace mbed
+//////////////////////////  printf redirection to serial END //////////////////////////
 #else
 
 #include <chrono>
