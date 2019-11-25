7102a8a6fa6615ca06cd39f5cecb64043993c59d
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-25 20:38:13 +0100

    small changes

--- python/moving_mean.py
+++ python/moving_mean.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-filename = 'beacons_record_1.txt'
+filename = 'beacons_record_1.csv'
 file = open(filename)
 data = file.readlines()
 rssi = [[float(s) for s in line.strip().split(',')[:3]] for line in data]
 rssi = np.array(rssi)
-N = int(sys.argv[1])
+N = 50
+if len(sys.argv)>1:
+    N = int(sys.argv[1])
 first = np.convolve(rssi[:,0], np.ones((N,))/N, mode='valid')
 second = np.convolve(rssi[:,1], np.ones((N,))/N, mode='valid')
 third = np.convolve(rssi[:,2], np.ones((N,))/N, mode='valid')
